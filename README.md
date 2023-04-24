d1e7c936102e4396e771fef27f589846f52adeaf
mibi88 <mibikart@gmail.com>
2023-04-24 19:38:31 +0200

    V.1.1 of TOXIC !

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # TOXIC
 A discord bot that completes every message.
-Used this french dictionnary https://joliesmaths.fr/wp-content/uploads/liste_francais_sans_accents.txt
+Used this french dictionnary https://joliesmaths.fr/wp-content/uploads/liste_francais_sans_accents.txt and a lowercase version of this english dictionnary https://gist.github.com/LuaDemon/af82d3e862f136e3c0e75dd9442f6a6f
--- toxic.py
+++ toxic.py
@@ -1,6 +1,7 @@
 import discord
+import random
 
-TOKEN = "token"
+TOKEN = "MTEwMDA0MDU2MjUxOTg0Mjg4Nw.GqCQHi.bEwMB2qGnIfgg4j5Urx6tLKk9Wnc91FbZnd-h8"
 
 intents = discord.Intents.default()  
 intents.message_content = True
@@ -12,23 +13,25 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    print("event")
     if message.author == client.user:
         return
     with open("liste_francais_sans_accents.txt", "r") as file:
         data = file.read()
+    with open("dict_ang2.txt", "r") as file:
+        data += '\n' + file.read()
     data = data.split('\n')
     s = message.content
     o = ""
     for w in s.split(' '):
+        p = []
         f = 0
         for i in data:
             if len(i)>len(w):
                 if i[0:len(w)] == w.strip().lower():
-                    o += i + ' '
+                    p.append(i)
                     f = 1
-                    break
         if not f: o += w + ' '
+        else: o += random.choice(p) + ' '
     await message.channel.send(o)
 
 client.run(TOKEN)
